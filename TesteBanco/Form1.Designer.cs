f215ed5974bea42297f77cfadcb53ce96ab80ece
sadokael <66951231+sadokael@users.noreply.github.com>
2021-01-26 22:01:01 +0000

    1st Commit

---
+++ TesteBanco/Form1.Designer.cs
@@ -0,0 +1,250 @@
+﻿namespace TesteBanco
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.LabelValor = new System.Windows.Forms.Label();
+            this.caixaNumero = new System.Windows.Forms.TextBox();
+            this.caixaNome = new System.Windows.Forms.TextBox();
+            this.caixaIdade = new System.Windows.Forms.TextBox();
+            this.LabelNumero = new System.Windows.Forms.Label();
+            this.LabelNome = new System.Windows.Forms.Label();
+            this.LabelIdade = new System.Windows.Forms.Label();
+            this.caixaPesId = new System.Windows.Forms.TextBox();
+            this.LabelId = new System.Windows.Forms.Label();
+            this.labelPesNumero = new System.Windows.Forms.Label();
+            this.labelPesNome = new System.Windows.Forms.Label();
+            this.labelPesIdade = new System.Windows.Forms.Label();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.btnGuardar = new System.Windows.Forms.Button();
+            this.btnPesquisa = new System.Windows.Forms.Button();
+            this.labelGetValue = new System.Windows.Forms.Label();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // LabelValor
+            // 
+            this.LabelValor.AutoSize = true;
+            this.LabelValor.Location = new System.Drawing.Point(3, 0);
+            this.LabelValor.Name = "LabelValor";
+            this.LabelValor.Size = new System.Drawing.Size(34, 13);
+            this.LabelValor.TabIndex = 0;
+            this.LabelValor.Text = "Valor:";
+            // 
+            // caixaNumero
+            // 
+            this.caixaNumero.Location = new System.Drawing.Point(88, 51);
+            this.caixaNumero.Name = "caixaNumero";
+            this.caixaNumero.Size = new System.Drawing.Size(100, 20);
+            this.caixaNumero.TabIndex = 2;
+            // 
+            // caixaNome
+            // 
+            this.caixaNome.Location = new System.Drawing.Point(88, 93);
+            this.caixaNome.Name = "caixaNome";
+            this.caixaNome.Size = new System.Drawing.Size(100, 20);
+            this.caixaNome.TabIndex = 3;
+            // 
+            // caixaIdade
+            // 
+            this.caixaIdade.Location = new System.Drawing.Point(88, 132);
+            this.caixaIdade.Name = "caixaIdade";
+            this.caixaIdade.Size = new System.Drawing.Size(100, 20);
+            this.caixaIdade.TabIndex = 4;
+            // 
+            // LabelNumero
+            // 
+            this.LabelNumero.AutoSize = true;
+            this.LabelNumero.Location = new System.Drawing.Point(3, 48);
+            this.LabelNumero.Name = "LabelNumero";
+            this.LabelNumero.Size = new System.Drawing.Size(44, 13);
+            this.LabelNumero.TabIndex = 5;
+            this.LabelNumero.Text = "Número";
+            // 
+            // LabelNome
+            // 
+            this.LabelNome.AutoSize = true;
+            this.LabelNome.Location = new System.Drawing.Point(3, 90);
+            this.LabelNome.Name = "LabelNome";
+            this.LabelNome.Size = new System.Drawing.Size(35, 13);
+            this.LabelNome.TabIndex = 6;
+            this.LabelNome.Text = "Nome";
+            // 
+            // LabelIdade
+            // 
+            this.LabelIdade.AutoSize = true;
+            this.LabelIdade.Location = new System.Drawing.Point(3, 129);
+            this.LabelIdade.Name = "LabelIdade";
+            this.LabelIdade.Size = new System.Drawing.Size(34, 13);
+            this.LabelIdade.TabIndex = 7;
+            this.LabelIdade.Text = "Idade";
+            // 
+            // caixaPesId
+            // 
+            this.caixaPesId.Location = new System.Drawing.Point(333, 21);
+            this.caixaPesId.Name = "caixaPesId";
+            this.caixaPesId.Size = new System.Drawing.Size(77, 20);
+            this.caixaPesId.TabIndex = 8;
+            // 
+            // LabelId
+            // 
+            this.LabelId.AutoSize = true;
+            this.LabelId.Location = new System.Drawing.Point(261, 18);
+            this.LabelId.Name = "LabelId";
+            this.LabelId.Size = new System.Drawing.Size(63, 26);
+            this.LabelId.TabIndex = 10;
+            this.LabelId.Text = "Introduza o Número";
+            // 
+            // labelPesNumero
+            // 
+            this.labelPesNumero.AutoSize = true;
+            this.labelPesNumero.Location = new System.Drawing.Point(333, 48);
+            this.labelPesNumero.Name = "labelPesNumero";
+            this.labelPesNumero.Size = new System.Drawing.Size(47, 13);
+            this.labelPesNumero.TabIndex = 11;
+            this.labelPesNumero.Text = "Número:";
+            // 
+            // labelPesNome
+            // 
+            this.labelPesNome.AutoSize = true;
+            this.labelPesNome.Location = new System.Drawing.Point(333, 90);
+            this.labelPesNome.Name = "labelPesNome";
+            this.labelPesNome.Size = new System.Drawing.Size(38, 13);
+            this.labelPesNome.TabIndex = 12;
+            this.labelPesNome.Text = "Nome:";
+            // 
+            // labelPesIdade
+            // 
+            this.labelPesIdade.AutoSize = true;
+            this.labelPesIdade.Location = new System.Drawing.Point(333, 129);
+            this.labelPesIdade.Name = "labelPesIdade";
+            this.labelPesIdade.Size = new System.Drawing.Size(34, 13);
+            this.labelPesIdade.TabIndex = 13;
+            this.labelPesIdade.Text = "Idade";
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 5;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 32.95019F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 67.0498F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 72F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 83F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 92F));
+            this.tableLayoutPanel1.Controls.Add(this.LabelValor, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.labelPesIdade, 3, 4);
+            this.tableLayoutPanel1.Controls.Add(this.labelPesNome, 3, 3);
+            this.tableLayoutPanel1.Controls.Add(this.LabelNumero, 0, 2);
+            this.tableLayoutPanel1.Controls.Add(this.labelPesNumero, 3, 2);
+            this.tableLayoutPanel1.Controls.Add(this.LabelNome, 0, 3);
+            this.tableLayoutPanel1.Controls.Add(this.caixaPesId, 3, 1);
+            this.tableLayoutPanel1.Controls.Add(this.LabelId, 2, 1);
+            this.tableLayoutPanel1.Controls.Add(this.LabelIdade, 0, 4);
+            this.tableLayoutPanel1.Controls.Add(this.caixaNumero, 1, 2);
+            this.tableLayoutPanel1.Controls.Add(this.caixaIdade, 1, 4);
+            this.tableLayoutPanel1.Controls.Add(this.caixaNome, 1, 3);
+            this.tableLayoutPanel1.Controls.Add(this.btnGuardar, 1, 5);
+            this.tableLayoutPanel1.Controls.Add(this.btnPesquisa, 4, 1);
+            this.tableLayoutPanel1.Controls.Add(this.labelGetValue, 1, 0);
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(86, 182);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 6;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 38.23529F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 61.76471F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 42F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 39F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 45F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 81F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(506, 256);
+            this.tableLayoutPanel1.TabIndex = 14;
+            // 
+            // btnGuardar
+            // 
+            this.btnGuardar.Location = new System.Drawing.Point(88, 177);
+            this.btnGuardar.Name = "btnGuardar";
+            this.btnGuardar.Size = new System.Drawing.Size(75, 23);
+            this.btnGuardar.TabIndex = 14;
+            this.btnGuardar.Text = "Guardar";
+            this.btnGuardar.UseVisualStyleBackColor = true;
+            this.btnGuardar.Click += new System.EventHandler(this.btnGuardar_Click);
+            // 
+            // btnPesquisa
+            // 
+            this.btnPesquisa.Location = new System.Drawing.Point(416, 21);
+            this.btnPesquisa.Name = "btnPesquisa";
+            this.btnPesquisa.Size = new System.Drawing.Size(75, 23);
+            this.btnPesquisa.TabIndex = 15;
+            this.btnPesquisa.Text = "Pesquisar";
+            this.btnPesquisa.UseVisualStyleBackColor = true;
+            this.btnPesquisa.Click += new System.EventHandler(this.btnPesquisa_Click);
+            // 
+            // labelGetValue
+            // 
+            this.labelGetValue.AutoSize = true;
+            this.labelGetValue.Location = new System.Drawing.Point(88, 0);
+            this.labelGetValue.Name = "labelGetValue";
+            this.labelGetValue.Size = new System.Drawing.Size(29, 13);
+            this.labelGetValue.TabIndex = 16;
+            this.labelGetValue.Text = "label";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.tableLayoutPanel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label LabelValor;
+ 
+        private System.Windows.Forms.TextBox caixaNumero;
+        private System.Windows.Forms.TextBox caixaNome;
+        private System.Windows.Forms.TextBox caixaIdade;
+        private System.Windows.Forms.Label LabelNumero;
+        private System.Windows.Forms.Label LabelNome;
+        private System.Windows.Forms.Label LabelIdade;
+        private System.Windows.Forms.TextBox caixaPesId;
+  
+        private System.Windows.Forms.Label LabelId;
+        private System.Windows.Forms.Label labelPesNumero;
+        private System.Windows.Forms.Label labelPesNome;
+        private System.Windows.Forms.Label labelPesIdade;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.Button btnGuardar;
+        private System.Windows.Forms.Button btnPesquisa;
+        private System.Windows.Forms.Label labelGetValue;
+    }
+}
+
---
+++ TesteBanco/Form1.cs
@@ -0,0 +1,193 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TesteBanco
+{
+    public partial class Form1 : Form
+    {
+
+        //Objeto Funcionário
+        public class Funcionario
+        {
+            public int numero;
+            public string nome;
+            public int idade;
+
+        }
+        
+        //array de objetos, limitada a 10
+        public Funcionario[] lista = new Funcionario[10];
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            //atribui o valor no load do formulário
+            labelGetValue.Text = getValue();
+            listInit();
+        }
+
+        //inicializa a lista com valores padrão, para me facilitar as validações na inserção
+        public void listInit() 
+        {
+            for(int i=0; i<lista.Length; i++)
+            {
+                Funcionario f = new Funcionario();
+                f.numero = 0;
+                f.nome = "";
+                f.idade = 0;
+                lista[i] = f;
+            }
+        }
+        public string getValue()
+        {
+            //https://www.codeproject.com/tips/858775/csharp-website-html-content-parsing-or-how-to-get <-- Onde descobri o HtmlAgilityPack
+
+            string HTML;
+
+            //String de retorno
+            string eurValue;
+
+            using (var wc = new WebClient())
+            {
+                HTML = wc.DownloadString("https://www.bportugal.pt/conversor-moeda?mlid=604");
+            }
+            var doc = new HtmlAgilityPack.HtmlDocument();
+            doc.LoadHtml(HTML);
+
+
+            //https://www.w3schools.com/xml/xpath_intro.asp  <-- W3 Schools, procurei como selecionar a div específica, porque não tinha id, descobri xpath's
+            
+            return eurValue = doc.DocumentNode.SelectNodes("//*[@id=\"edit-result\"]/div[1]/div/div")[0].InnerText;
+            
+        }
+        
+        public void addFuncionario(int n)
+        {   
+            // Atribui os valores nas caixas ás propriedades do funcionário, numéricos parsed a int
+            Funcionario f = new Funcionario();
+            f.numero = int.Parse(caixaNumero.Text);
+            f.nome = caixaNome.Text;
+            f.idade = int.Parse(caixaIdade.Text);
+            lista[n] = f;
+           
+        }
+
+        private void btnGuardar_Click(object sender, EventArgs e)
+        {
+            //faltam validações mais refinadas, para exemplo confirma que as caixas não estão vazias
+            //pode ocorrer exceção se o número ou idade não forem numéricos
+            if (caixaNumero.Text != "" && caixaNome.Text != "" && caixaIdade.Text != "")
+            {   
+                //atribui o numero a uma variável
+                int check = int.Parse(caixaNumero.Text);
+
+                //Boleano para decidir a messageBox a mostrar
+                bool inseriu = false;
+
+                //primeiro ciclo, para confirmar se o numero a introduzir é novo
+                for (int j = 0; j < lista.Length; j++)
+                {
+                    if(lista[j].numero == check)
+                    {
+                        //Se o número já existe
+                        MessageBox.Show(
+                                "Um funcionário com esse número já existe!",
+                                "Funcionarios",
+                                MessageBoxButtons.OK,
+                                MessageBoxIcon.Information);
+                    }
+                    else
+                    {
+                        //segundo ciclo que confirma se existem funcionários com numero 0
+                        for (int i = 0; i < lista.Length; i++)
+                        {
+                            //se o numero introduzido foi diferente do padrão (0)
+                            if (lista[i].numero != 0)
+                            {
+                                //adiciona o funcionário
+                                addFuncionario(check);
+                                //dá trigger ao booleano para mostrar a mensagem de sucesso
+                                inseriu = true;
+                                //e sai do ciclo for
+                                break;
+                            }
+                            else
+                            {
+                                //senão dá trigger á mensagem de erro
+                                inseriu = false;
+
+                            }
+                        }
+                        if (inseriu == false)
+                        {
+                            //Esta mensagem assume que já não existem funcionários com numero "0"
+                            MessageBox.Show(
+                                    "Já têm 10 funcionários!",
+                                    "Funcionarios",
+                                    MessageBoxButtons.OK,
+                                    MessageBoxIcon.Information);
+                        }
+                        else
+                        {
+                            //mensagem de sucesso
+                            caixaNumero.Text = "";
+                            caixaNome.Text = "";
+                            caixaIdade.Text = "";
+                            MessageBox.Show(
+                                        "Funcionario inserido com sucesso",
+                                        "Funcionarios",
+                                        MessageBoxButtons.OK,
+                                        MessageBoxIcon.Information);
+                        }
+                    }
+                }
+                    
+            }
+            else
+            {
+                //caso algum campo esteja por preencher
+                MessageBox.Show(
+                            "Os campos não foram totalmente inseridos",
+                            "Funcionarios",
+                            MessageBoxButtons.OK,
+                            MessageBoxIcon.Warning);
+            }
+        }
+
+        private void srcFuncionario()
+        {
+            //atribui o valor da caixa para uma variavel Int...
+            int n = int.Parse(caixaPesId.Text);
+
+            for (int i = 0; i < lista.Length; i++)
+            {
+                //...e precorre a array até encontrar o ID correcto
+                if (lista[i].numero == n)
+                {
+                    //concatenação para poupar a quantidade de labels
+                    labelPesNumero.Text = "Número: " +lista[i].numero.ToString();
+                    labelPesNome.Text = "Nome: " + lista[i].nome.ToString();
+                    labelPesIdade.Text = "Idade: " + lista[i].idade.ToString();               
+                }
+            }
+        }
+
+        private void btnPesquisa_Click(object sender, EventArgs e)
+        {
+            srcFuncionario();
+        }
+
+   
+    }
+}
+
+  
